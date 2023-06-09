e5fbc5ab661e166c0a371c0522606d53caec21d7
CHANDRAMOULIJOKADA <109852342+CHANDRAMOULIJOKADA@users.noreply.github.com>
2023-06-09 19:19:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	
 	let containsKey = 'key' in sampleObject;
-	if(containsKey===true) return true;
+	if(containsKey) return true;
 	else false;
 	}
 }
 
 // Do not change the code below
-//const key = prompt("Enter Key.");
-//alert(hasKey(key));
+const key = prompt("Enter Key.");
+alert(hasKey(key));
+
